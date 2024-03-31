b25aa278b29a2f9159a0c77a3b2db97b29c2fa67
AgrebiOmar123 <oagrebi614@gmail.com>
2024-03-31 10:59:31 +0100

    v2

---
+++ resources/views/test.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+@section('title')
+Test View
+@endsection
+@section('contentheader')
+Test 
+@endsection
+@section('contentheaderlink')
+<a href="#">Link</a>
+@endsection
+@section('contentheaderactive')
+View
+@endsection
+@section('content')
+test viewhh
+@endsection
